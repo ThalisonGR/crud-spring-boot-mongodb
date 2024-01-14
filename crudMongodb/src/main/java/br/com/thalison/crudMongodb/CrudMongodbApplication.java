76ae51f96233703f5f16ca335bfd7613c0336988
Thalison <Tec.thalison@gmail.com>
2024-01-14 01:22:44 -0300

    :tada: Initial commit

---
+++ crudMongodb/src/main/java/br/com/thalison/crudMongodb/CrudMongodbApplication.java
@@ -0,0 +1,14 @@
+package br.com.thalison.crudMongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMongodbApplication.class, args);
+	}
+
+}
+
